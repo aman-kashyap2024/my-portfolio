f2d2aac0d59db84d194ea7fad1814b9dbf1f9dcd
aman-kashyap2024 <amankashyap10145@gmail.com>
2024-05-31 10:36:11 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", " UI/UX Designer" , "Frontend Developer"] ,
+    typeSpeed: 90,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
